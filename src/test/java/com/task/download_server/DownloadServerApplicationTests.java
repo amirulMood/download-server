0c76cf0f843538024c89b667fb0da7f5942bb966
amiruldanielcs <106524830+amirulMood@users.noreply.github.com>
2025-10-29 22:27:49 +0800

    Initial commit

---
+++ src/test/java/com/task/download_server/DownloadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.download_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
